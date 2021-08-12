ea024c3614021ccd96b3f410cc4fdb0073bdf8e2
KavinduRankothge-tech <kavindunilhan.rankothge@students.mq.edu.au>
2021-08-12 17:11:38 +0530

    Updated Book model to add Id attribute to bookId

--- Rest/src/main/java/com/example/demo/model/Book.java
+++ Rest/src/main/java/com/example/demo/model/Book.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;
@@ -12,7 +13,7 @@ import lombok.ToString;
 
 @Document(collection ="Book")
 public class Book {
-	
+	@Id
 	private int id;
 	private String bookName;
 	private String authorName;
